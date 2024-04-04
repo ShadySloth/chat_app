e179b5e7e1d8ddfa7b88936474e0448d191d8baf
ShadySloth <pedant01@easv365.dk>
2024-04-04 09:48:19 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_chat_app/cubits/profiles/profiles_cubit.dart';
+import 'package:my_chat_app/utils/constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:my_chat_app/pages/splash_page.dart';
+import 'package:my_chat_app/secret.dart' as secret;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: secret.url,
+    anonKey: secret.anonKey,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<ProfilesCubit>(
+      create: (context) => ProfilesCubit(),
+      child: MaterialApp(
+        title: 'SupaChat',
+        debugShowCheckedModeBanner: false,
+        theme: appTheme,
+        home: const SplashPage(),
+      ),
+    );
+  }
+}
---
+++ lib/secret.dart
@@ -0,0 +1,4 @@
+
+const url = 'https://wkjurjbfmixuawociwhf.supabase.co';
+const anonKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndranVyamJmbWl4dWF3b2Npd2hmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwNDU2NzEsImV4cCI6MjAyNzYyMTY3MX0.rOsQMdSIIcEnXhmyvhV7oPmB_tDx9sl5VGYIM-Pd-3o';
